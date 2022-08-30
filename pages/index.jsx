934ae38f6ebc31594c7d08fc0b89a99ffd038a52
1DVwbp <fmxg6223@gmail.com>
2022-08-30 20:26:57 +0800

    fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,7 +12,7 @@ const Home = () => {
   const fetchNFTs = async() => {
     let nfts; 
     console.log("fetching nfts");
-    const api_key = '61983395XEfyWiGVMB3LQRrxA9ZA2_dO'
+    const api_key = '7jRYUjyO6d48nMuyEUn_Z2Pmps_Kw-Ax'
 
     const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTs/`;
     var requestOptions = {
@@ -41,7 +41,7 @@ const Home = () => {
       var requestOptions = {
         method: 'GET'
       };
-      const api_key = '61983395XEfyWiGVMB3LQRrxA9ZA2_dO'
+      const api_key = '7jRYUjyO6d48nMuyEUn_Z2Pmps_Kw-Ax'
 
       const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTsForCollection/`;
       const fetchURL = `${baseURL}?contractAddress=${collection}&withMetadata=${"true"}`;
